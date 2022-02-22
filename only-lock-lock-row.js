1223df87268154992ff230a83ad4bb6412e9caca
frozenwizard <172203+frozenwizard@users.noreply.github.com>
2022-02-21 20:55:50 -0600

    Autofixing js files and amending rules.

--- only-lock-lock-row.js
+++ only-lock-lock-row.js
@@ -1,6 +1,9 @@
+/**
+ *  Only Lock Lock custom entity
+ */
 class OnlyLockLockRow extends Polymer.Element {
-    static get template() {
-        return Polymer.html`
+  static get template() {
+    return Polymer.html`
           <style include="iron-flex iron-flex-alignment"></style>
           <style>
             mwc-button {
@@ -11,53 +14,52 @@ class OnlyLockLockRow extends Polymer.Element {
           </style>
           <hui-generic-entity-row hass="[[hass]]" config="[[_config]]"
             <div class="horizontal justified layout">
-              <state-info hass="[[hass]]" state-obj="[[stateObj]]"></state-info>           
+              <state-info hass="[[hass]]" state-obj="[[stateObj]]"></state-info>        
               <mwc-button on-click="_callLockService">Lock</mwc-button>
             </div>
           </hui-generic-entity-row>
        
-        `
-    }
+        `;
+  }
 
 
-    static get properties() {
-        return {
-            hass: Object,
-            stateObj: {
-                type: Object,
-                observer: "_stateObjChanged",
-            }
-        };
-    }
+  static get properties() {
+    return {
+      hass: Object,
+      stateObj: {
+        type: Object,
+        observer: '_stateObjChanged',
+      },
+    };
+  }
 
-    setConfig(config) {
-        this._config = {
-            tap_action: {
-                action: "none"
-            },
-            ...config
-        };
-    }
+  setConfig(config) {
+    this._config = {
+      tap_action: {
+        action: 'none',
+      },
+      ...config,
+    };
+  }
 
 
-    displayName() {
-        return this._config.name || this.stateObj.attributes.friendly_name;
-    }
+  displayName() {
+    return this._config.name || this.stateObj.attributes.friendly_name;
+  }
 
-    _stateObjChanged(newVal) {
-        this.setProperties({
-            _stateObj: stateObj
-        });
+  _stateObjChanged(newVal) {
+    this.setProperties({
+      _stateObj: stateObj,
+    });
+  }
 
-    }
-
-    _callLockService(ev) {
-        ev.stopPropagation();
-        const data = {
-            entity_id: this._config.entity,
-        };
-        this.hass.callService("lock", "lock", data);
-    }
+  _callLockService(ev) {
+    ev.stopPropagation();
+    const data = {
+      entity_id: this._config.entity,
+    };
+    this.hass.callService('lock', 'lock', data);
+  }
 }
 
 customElements.define('only-lock-lock-row', OnlyLockLockRow);
