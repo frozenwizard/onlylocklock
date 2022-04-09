747e434abea30c64b3766ea6d3125e13497fdbc8
frozenwizard <172203+frozenwizard@users.noreply.github.com>
2022-04-09 14:36:40 -0500

    Adding only close cover and updating readme.

---
+++ only-close-cover-row.js
@@ -0,0 +1,65 @@
+/**
+ *  Only Close Cover custom entity
+ */
+class OnlyCloseCoverRow extends Polymer.Element {
+  static get template() {
+    return Polymer.html`
+          <style include="iron-flex iron-flex-alignment"></style>
+          <style>
+            mwc-button {
+              top: 3px;
+              height: 37px;
+              margin-right: -0.57em;
+            }
+          </style>
+          <hui-generic-entity-row hass="[[hass]]" config="[[_config]]"
+            <div class="horizontal justified layout">
+              <state-info hass="[[hass]]" state-obj="[[stateObj]]"></state-info>        
+              <mwc-button on-click="_callLockService">Close</mwc-button>
+            </div>
+          </hui-generic-entity-row>
+       
+        `;
+  }
+
+
+  static get properties() {
+    return {
+      hass: Object,
+      stateObj: {
+        type: Object,
+        observer: '_stateObjChanged',
+      },
+    };
+  }
+
+  setConfig(config) {
+    this._config = {
+      tap_action: {
+        action: 'none',
+      },
+      ...config,
+    };
+  }
+
+
+  displayName() {
+    return this._config.name || this.stateObj.attributes.friendly_name;
+  }
+
+  _stateObjChanged(newVal) {
+    this.setProperties({
+      _stateObj: stateObj,
+    });
+  }
+
+  _callLockService(ev) {
+    ev.stopPropagation();
+    const data = {
+      entity_id: this._config.entity,
+    };
+    this.hass.callService('cover', 'close', data);
+  }
+}
+
+customElements.define('only-close-cover-row', OnlyCloseCoverRow);
