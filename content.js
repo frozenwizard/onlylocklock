bcec2365a858e57589a196813bdbd83aedcc8637
frozenwizard <172203+frozenwizard@users.noreply.github.com>
2022-11-15 13:09:56 -0600

    Creating single import to load in all the files.

---
+++ content.js
@@ -0,0 +1,7 @@
+import {OnlyArmAlarmRow} from './only-arm-alarm-row.js';
+import {OnlyCloseCoverRow} from './only-close-cover-row.js';
+import {OnlyLockLockRow} from './only-lock-lock-row.js';
+
+customElements.define('only-close-cover-row', OnlyCloseCoverRow);
+customElements.define('only-arm-alarm-row', OnlyArmAlarmRow);
+customElements.define('only-lock-lock-row', OnlyLockLockRow);
